2550f8bc49ee9a3b8bead3e9ccc2a617b0fc8a1b
nauvalachid <nauvalachid@gmail.com>
2024-11-14 08:42:30 +0700

    first commit

---
+++ dependency.js
@@ -0,0 +1,6 @@
+import _ from "lodash";
+
+const source = "EKO KURNIAWAN KHANNEDY";
+const target = _.capitalize(source);
+
+console.info(target);
---
+++ export.js
@@ -0,0 +1,3 @@
+import { writeToFile } from "./write";
+
+writeToFile("hello.log","nauvalachid");
